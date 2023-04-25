314e84f74b300a7d11bd65a530a7c60dc36c1e6c
JasubiP <gameportal7@hotmail.com>
2023-04-24 21:12:17 -0600

    Change Footer and Nav

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,29 @@
 import styles from '@/styles/Footer.module.css'
-import Image from 'next/image'
+import { FaFacebookF } from 'react-icons/fa'
+import { BsTwitter } from 'react-icons/bs'
+import Link from 'next/link'
 
 export default function Footer(){
   return(
     <footer className={styles.footer}>
-      <a href="https://www.facebook.com/jasubip" 
-      className={styles.footer_center}
-      target='_blank'
-      >
-        <p>Con Amor Por</p> 
-        <img loading='lazy' src="/img/jasubip-logo.png" alt="Desarrollada por JasubiP" />
-      </a>
+      <div className={styles.footer_credits}>
+        <a 
+          href="https://www.facebook.com/jasubip" 
+          target='_blank'
+        >
+          <p>Con Amor Por</p> 
+          <img loading='lazy' src="/img/jasubip-logo.png" alt="Desarrollada por JasubiP" />
+        </a>
+      </div>
+      <ul className={styles.footer_center}>
+        <li>
+          <Link href={'/sobre-nosotros'} >Sobre Nosotros</Link>
+        </li>
+        <li>
+          <a href='https://www.facebook.com/profile.php?id=100092340722303' target='_blank'><FaFacebookF /></a>
+          <a href='https://twitter.com/NobodyCaresOffi' target='_blank'><BsTwitter /></a>
+        </li>
+      </ul>
     </footer>
   )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,18 +1,21 @@
 import styles from '@/styles/Nav.module.css'
 import Link from 'next/link'
+import { FaFacebookF } from 'react-icons/fa'
+import { BsTwitter } from 'react-icons/bs'
 
 export default function Nav(){
   return(
     <nav className={styles.nav}>
-      <Link className={styles.nav_title_container} href='/'>
-        <img src='/img/logo.png' alt='logo icon' />
-        <h2 className={styles.nav_title}><span>NOBODY</span>CARES </h2>
-      </Link>
-      <ul className={styles.menu}>
-        <li className={styles.menu_item}>
-          <Link href="/sobre-nosotros"> Sobre Nosotros</Link>
-        </li>
-      </ul>
+      <section className={styles.nav_center}>
+        <Link className={styles.nav_title_container} href='/'>
+          <img src='/img/logo.png' alt='logo icon' />
+          <h2 className={styles.nav_title}><span>NOBODY</span>CARES </h2>
+        </Link>
+        <div className={styles.social_network_container}>
+          <a href='https://www.facebook.com/profile.php?id=100092340722303' target='_blank'><FaFacebookF /></a>
+          <a href='https://twitter.com/NobodyCaresOffi' target='_blank'><BsTwitter /></a>
+        </div>
+      </section>
     </nav>
   )
 }
