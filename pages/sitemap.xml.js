c1de7c07d7273b1b268be2ba7184a26c4a74d1bd
JasubiP <gameportal7@hotmail.com>
2023-04-28 21:02:41 -0600

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,15 +1,12 @@
+// Importa el módulo "fs" para escribir el archivo sitemap.xml
+import fs from 'fs';
+
 const EXTERNAL_DATA_URL = 'https://unaopinionmas.vercel.app/posts';
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
      ${posts
        .map(({ id }) => {
          return `
@@ -24,10 +21,10 @@ function generateSiteMap(posts) {
 }
 
 function SiteMap() {
-  // getServerSideProps will do the heavy lifting
+  // getStaticProps will do the heavy lifting
 }
 
-export async function getServerSideProps({ res }) {
+export async function getStaticProps() {
   // We make an API call to gather the URLs for our site
   const request = await fetch(EXTERNAL_DATA_URL);
   const posts = await request.json();
@@ -35,10 +32,8 @@ export async function getServerSideProps({ res }) {
   // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(posts);
 
-  res.setHeader('Content-Type', 'text/xml');
-  // we send the XML to the browser
-  res.write(sitemap);
-  res.end();
+  // Write the sitemap.xml file to the public directory
+  fs.writeFileSync('public/sitemap.xml', sitemap);
 
   return {
     props: {},
