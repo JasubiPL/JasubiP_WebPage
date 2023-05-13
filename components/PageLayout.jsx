972b4fc66a17c70a310d27bd399d0a1c90f1216f
JasubiP <gameportal7@hotmail.com>
2023-05-12 22:09:58 -0600

    Added new post Calidad Laboral

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -36,10 +36,10 @@ export default function PageLayout({
       <meta property="twitter:description" content={description} />
 
       {/*  Etiquetas meta para redes sociales */}
-      <meta property="og:title" content={title} />
+      {/* <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image" content={`https://unaopinionmas.vercel.app${shareImage}`} /> */}
       <meta property="og:type" content="article" />
-      <meta property="og:image" content={`https://unaopinionmas.vercel.app${shareImage}`} />
       <meta property="og:url" content="https://unaopinionmas.vercel.app/" />
       <meta property="og:site_name" content="Nobody Cares " />
       <meta property="og:locale" content="es_ES" />
