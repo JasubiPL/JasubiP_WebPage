c5d671045a378021c5e078012948b4f14b9695c8
JasubiPL <gameportal7@hotmail.com>
2023-05-25 11:02:13 -0600

    Add Google analitics

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,11 @@ class MyDocument extends Document {
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TR2XNL3');`,
             }}
           />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z9SNGY0CBB"></script>
+          <script 
+            dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-Z9SNGY0CBB');`
+            }}
+          />
         </Head>
         <body>
         {/* Google Tag Manager (noscript) */}
