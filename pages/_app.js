4ed71fa7b216b4258a9dca529eb36c4c0274f30f
JasubiPL <gameportal7@hotmail.com>
2023-05-25 17:14:33 -0600

    added analitics plugin

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.css'
 import SessionProvider from '@/hooks/SessionContext.jsx'
 
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <SessionProvider>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   )
 }
