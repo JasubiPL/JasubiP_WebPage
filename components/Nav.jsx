44c600ac1cb884d7d4bcae542bc94020409f23d8
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-01-24 18:45:51 -0600

    Se añadio menu de secciones

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,10 @@
 import styles from '@/styles/Nav.module.css'
 import Link from 'next/link'
 import { SocialNetwork } from './SocialNetwors'
+import { useRouter } from 'next/router'
 
 export default function Nav(){
+  const path = useRouter().pathname
 
   return(
     <nav id="header-nav" className={styles.nav}>
@@ -10,7 +12,20 @@ export default function Nav(){
         <Link className={styles.nav_title_container} href='/'>
           <img src='/img/imagotipo-Blanco.png' alt='logo icon' />
         </Link>
-        <SocialNetwork />
+        
+        <ul className='flex gap-2 w-full py-2 justify-center order-1 md:order-0 md:w-auto'>
+          <li className={`py-1 px-4 transition-all hover:bg-accent_100  rounded-full ${path === '/' ? 'bg-accent_100' :''}`}>
+            <Link href='/'>Inicio</Link>
+          </li>
+          <li className={`py-1 px-4 transition-all hover:bg-accent_100  rounded-full ${path.includes('/posts')  ? 'bg-accent_100' :''}`}>
+            <Link href='/posts'>Blog</Link>
+          </li>
+          <li className={`py-1 px-4 transition-all hover:bg-accent_100  rounded-full ${path.includes('/streaming')  ? 'bg-accent_100' :''}`}>
+            <Link href='/'>Streaming</Link>
+          </li>
+        </ul>
+
+        <SocialNetwork className='md:order-2'/>
       </section>
     </nav>
   )
--- components/SocialNetwors.jsx
+++ components/SocialNetwors.jsx
@@ -2,9 +2,9 @@ import { FaGithub, FaTwitch } from 'react-icons/fa'
 import { BsTwitterX } from "react-icons/bs";
 import styles from '../styles/SocialNetworks.module.css'
 
-export const SocialNetwork = () =>{
+export const SocialNetwork = ({ className }) =>{
   return(
-    <div className={styles.social_network_container}>
+    <div className={`${styles.social_network_container} ${ className }`}>
       <a href='https://x.com/jasubip' target='_blank'><BsTwitterX /></a>
       <a href='https://www.twitch.com/jasubip' target='_blank'><FaTwitch /></a>
       <a href="http://github.com/JasubiPL" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
